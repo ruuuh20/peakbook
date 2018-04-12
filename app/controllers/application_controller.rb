f33a2178e48b9250b13af1a65ad24f7b85fd4045
ruuuh20 <pkim2320@gmail.com>
2018-04-12 10:53:37 -0400

    fixing authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
   request.env['omniauth.origin'] || root_path
 end
+
+def must_be_admin
+  unless current_user.admin?
+    redirect_to root_path, :alert => "Access denied"
+  end
+end
+
+def must_be_admin_or_teacher
+  unless current_user.admin? || current_user.teacher?
+    redirect_to root_path, :alert => "Access denied"
+  end
+end
 end
